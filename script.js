d0a4f024f7a0639efa391892cc64b190bf79466c
udin-imad <udinwebdev@gmail.com>
2023-10-20 06:48:58 +0700

    initialize variable and DOM selection

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initialize Variable
+let firstNum = '';
+let secondNum = '';
+let operator = null;
+
+// DOM selection
+const currentDisplay = document.querySelector('#current-display');
+const latestDisplay = document.querySelector('#latest-display');
+const numberButton = document.querySelectorAll('.number');
+const operatorButton = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('#equal');
+const pointButton = document.querySelector('#point');
+const deleteButton = document.querySelector('#delete');
+const clearButton = document.querySelector('#clear');
